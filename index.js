949fa857ba446f1c8a22f9093cd771a6654d0eec
panpai04 <irpan.pauzi16@gmail.com>
2024-03-16 22:27:56 +0700

    2nd

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const PORT = 6789;
+const server = express();
+server.use(express.urlencoded());
+
+// todo : Controller
+const lihat_history = require("./controller/lihat-history");
+const hitungLuas = require("../controller/rumus-luas")
+const hitungKeliling =require("../room-2/controller/rumus-keliling")
+
+// todo : Routing
+server.get("/lihat-history", lihat_history);
+
+server.post('/hitung-luas', hitungLuas)
+
+server.post('/hitung-keliling', hitungKeliling)
+
+server.listen(PORT, function(){
+  console.log(`Server berjalan di localhost:${PORT}`);
+});
